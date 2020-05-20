d6123627cb46dba1c0873445b205e76b5dfc553f
Eric Mikulin <ericm99@gmail.com>
2020-05-20 16:57:11 -0700

    Add a few more options!

--- toasted_mirrors_sync.sh
+++ toasted_mirrors_sync.sh
@@ -6,6 +6,18 @@ LOCKFILE=/tmp/rsync-mirrors.lock
 RSYNC=/usr/bin/rsync
 RSYNC_OPTIONS="-rvtlH --progress --delay-updates --delete-after --safe-links --temp-dir=$DESTPATH/.rsynctemp"
 
+DEBIAN_IGNORE=" \
+    --exclude '*powerpc.deb' --exclude '*powerpc.udeb' \
+    --exclude '*ppc64el.deb' --exclude '*ppc64el.udeb' \
+    --exclude '*s390x.deb' --exclude '*s390x.udeb' \
+    --exclude '*riscv64.deb' --exclude '*riscv64.udeb' \
+    --exclude '*i386.deb' --exclude '*i386.udeb' \
+    --exclude '*mips.deb' --exclude '*mips.udeb' \
+    --exclude '*mips64el.deb' --exclude '*mips64el.udeb' \
+    --exclude '*mipsel.deb' --exclude '*mipsel.udeb' \
+    --exclude '*mipsel.deb' --exclude '*mipsel.udeb' \
+"
+
 LOG_PREFIX="Toasted Mirrors:"
 
 fixpermissions() {
@@ -23,14 +35,24 @@ synchronize() {
 
     echo "$LOG_PREFIX Ubuntu-ports sync"
     $RSYNC $RSYNC_OPTIONS \
-        --exclude "*powerpc.deb" --exclude "*ppc64el.deb" --exclude "*s390x.deb" --exclude "*riscv64.deb" \
-        --exclude "*powerpc.udeb" --exclude "*ppc64el.udeb" --exclude "*s390x.udeb" --exclude "*riscv64.udeb" \
+        $DEBIAN_IGNORE
         rsync://ports.ubuntu.com/ubuntu-ports/ "$DESTPATH/ubuntu-ports"
     sleep 2
 
     echo "$LOG_PREFIX Raspbian sync"
     $RSYNC $RSYNC_OPTIONS \
         rsync://muug.ca/mirror/raspbian/raspbian/ "$DESTPATH/raspbian"
+    sleep 2
+
+    echo "$LOG_PREFIX Arch Linux Sync"
+    $RSYNC $RSYNC_OPTIONS \
+        rsync://mirror.csclub.uwaterloo.ca/archlinux/ "$DESTPATH/archlinux"
+    sleep 2
+
+    echo "$LOG_PREFIX Debian Sync"
+    $RSYNC $RSYNC_OPTIONS \
+        $DEBIAN_IGNORE
+        rsync://ftp.ca.debian.org/debian/ "$DESTPATH/debian"
     sleep 2
 
     # Only need to run this every so often
